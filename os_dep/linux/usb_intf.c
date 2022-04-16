723b9dbadd21cf3b8b8c6675734fb69576f3a3d9
kamiokan <spherenov@gmail.com>
2022-04-16 21:43:52 +0900

    ELECOM WDC-433SU2M2をLinuxMint20.3で使えるように追記した

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -310,6 +310,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x0bda, 0x0823),.driver_info = RTL8821}, /* I-O DATA - WNPU583B */
 	{USB_DEVICE(0x2357, 0x011e),.driver_info = RTL8821}, /* TP-Link - Archer T2U Nano, AC600 */
 	{USB_DEVICE(0x2357, 0x0120),.driver_info = RTL8821}, /* TP-Link - Archer T2U Plus, AC600 */
+        {USB_DEVICE(0x056e, 0x400e),.driver_info = RTL8821}, /* WDC-433SU2M2 */
 #endif
 
 #ifdef CONFIG_RTL8192E
